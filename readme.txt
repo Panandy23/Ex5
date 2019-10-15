d12231b487fdf52d3578a10238fda12c5f68a5f0
Andrzej Pawlowski <panandy@poczta.onet.pl>
2019-10-15 11:30:03 +0200

    program prawidłowo wyznacza miejsca zerowe funkcji kwadratowej

---
+++ readme.txt
@@ -0,0 +1,6 @@
+Napisz program, który oblicza pierwiastki równania kwadratowego
+ax2+bx+c = 0 z wykorzystaniem instrukcji warunkowej
+if, gdzie zmienne a, b i c to liczby rzeczywiste wprowadzane
+z klawiatury. Dla zmiennych a, b, c, x1 oraz x2 należy przyjąć
+format wyświetlania ich na ekranie z dokładnością do dwóch
+miejsc po przecinku.
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        double a, b, c;
+        double x1, x2;
+        double delta;
+
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Podaj zmienną a: ");
+        a = scanner.nextDouble();
+        System.out.println("Podaj zmienną b: ");
+        b = scanner.nextDouble();
+        System.out.println("Podaj zmienną c: ");
+        c = scanner.nextDouble();
+
+        delta = Math.pow(b, 2) - 4*a*c;
+
+        System.out.printf("Delta wynosi: " + "%2.2f", delta);
+        System.out.println(" ");
+
+        if (delta < 0){
+
+            System.out.println("Brak miejsc zerowych funkcji");
+
+        } else if (delta == 0){
+
+            x1 = -b / 2*a;
+
+            System.out.printf("Funkcja ma jedno miejsce zerowe x1 = " + "%2.2f", x1);
+
+        } else {
+
+            x1 = (-b - Math.sqrt(delta))/2*a;
+            x2 = (-b + Math.sqrt(delta))/2*a;
+
+            System.out.println("Funkcja ma dwa miejsca zerowe: ");
+            System.out.printf("x1 = " + "%2.2f", x1);
+            System.out.printf(" ,x2 = " + "%2.2f", x2);
+
+        }
+
 
     }
 }
